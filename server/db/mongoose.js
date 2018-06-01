585d04362c62509967befb5a2f2409c95ae47715
jsc215 <jeffcomertennis@gmail.com>
2018-06-01 15:09:02 -0400

    needs the URI for prod

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/goji-project');
+// need to have condition for using prod or locoal db
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/goji-project');
 module.exports = { mongoose };
